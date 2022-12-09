0c43d5170a6a3e01a49c43725725db9eef0a18c1
Ghifari80 <ghifariauliarutfi@gmail.com>
2022-12-09 16:59:08 +0700

    edit

--- Authentication/Program.cs
+++ Authentication/Program.cs
@@ -14,60 +14,90 @@
         }
         static void Main(string[] args)
         {
-            MainMenu();
-
-            int input = Convert.ToInt32(Console.ReadLine());
-
-            switch (input)
+            List<int> id = new List<int>();
+            List<string> first_name = new List<string>();
+            List<string> last_name = new List<string>();
+            List<string> username = new List<string>();
+            List<string> password = new List<string>();
+            int input2;
+            do
             {
-                case 1:
-                    Console.WriteLine();
-                    Console.Clear();
+                MainMenu();
+                int input = Convert.ToInt32(Console.ReadLine());
 
-                    //input first_name
-                    Console.Write("First Name : ");
-                    String first_name = Console.ReadLine();
+                switch (input)
+                {
+                    case 1:
+                        Console.Clear();
+                        Console.WriteLine();
+                        InputUser(id, first_name, last_name, password);
+                        break;
+                    case 2:
+                        Console.Clear();
+                        TampilUser(id, first_name, last_name, password);
+                        break;
+                    case 3:
+                        Console.Clear();
+                        Console.WriteLine("==CARI AKUN==");
+                        Console.Write("Masukan Nama : ");
+                        String name = Console.ReadLine();
+                        break;
+                    case 4:
+                        Console.Clear();
+                        Console.WriteLine("LOGIN");
+                        Console.Write("USERNAME : ");
+                        // input username
+                        username.Add(Console.ReadLine());
 
-                    //input last_name
-                    Console.Write("Last Name : ");
-                    String last_name = Console.ReadLine();
+                        Console.Write("PASSWORD : ");
+                        // input Password
+                        String pass = Console.ReadLine();
+                        break;
+                    case 5:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine();
+                        break;
+                }
+                Console.WriteLine("Kembali Ke Home?");
+                Console.WriteLine("1. Ya");
+                Console.WriteLine("2. tidak");
+                Console.Write("Masukan Pilihan : ");
+                input2 = Convert.ToInt32(Console.ReadLine());
+            } while (input2 != 2);
+        }
+        static void InputUser(List<int> id, List<string> first_name, List<string> last_name, List<string> password)
+        {
+            //input first_name
+            Console.Write("First Name : ");
+            first_name.Add(Console.ReadLine());
 
-                    //input password
+            //input last_name
+            Console.Write("Last Name : ");
+            last_name.Add(Console.ReadLine());
 
-                    Console.Write("Password : ");
-                    String password = Console.ReadLine();
-                    break;
-                case 2:
-                    Console.Clear();
-                    Console.WriteLine("==SHOW USER==");
-                    Console.WriteLine("=============");
-                    Console.WriteLine();
-                    Console.WriteLine("=============");
-                    break;
-                case 3:
-                    Console.Clear();
-                    Console.WriteLine("==CARI AKUN==");
-                    Console.Write("Masukan Nama : ");
-                    String name = Console.ReadLine();
-                    break;
-                case 4:
-                    Console.Clear();
-                    Console.WriteLine("LOGIN");
-                    Console.Write("USERNAME : ");
-                    // input username
-                    String username = Console.ReadLine();
+            //input password
+            Console.Write("Password : ");
+            password.Add(Console.ReadLine());
 
-                    Console.Write("PASSWORD : ");
-                    // input Password
-                    String pass = Console.ReadLine();
-                    break;
-                case 5:
-                    Environment.Exit(0);
-                    break;
-                default:
-                    Console.WriteLine();
-                    break;
+            //input id
+            int idinput = first_name.Count + 1;
+            id.Add(idinput);
+
+        }
+        static void TampilUser(List<int> id, List<string> first_name, List<string> last_name, List<string> password)
+        {
+            Console.WriteLine("==SHOW USER==");
+            Console.WriteLine("=============");
+            for (int i = 0; i < first_name.Count; i++)
+            {
+                Console.WriteLine("ID       : " + id);
+                Console.WriteLine("Name     : " + first_name[i] + " " + last_name[i]);
+                Console.WriteLine("Username : " + last_name[i]);
+                Console.WriteLine("Password : " + password[i]);
             }
+            Console.WriteLine("=============");
         }
     }
 }

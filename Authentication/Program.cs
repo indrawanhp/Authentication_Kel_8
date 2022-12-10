4b15514c730ca87b38a577f666fc11397d31c4ed
Ghifari80 <ghifariauliarutfi@gmail.com>
2022-12-10 12:23:26 +0700

    progress

--- Authentication/Program.cs
+++ Authentication/Program.cs
@@ -15,7 +15,6 @@
         }
         static void Main(string[] args)
         {
-            List<int> id = new List<int>();
             List<string> first_name = new List<string>();
             List<string> last_name = new List<string>();
             List<string> username = new List<string>();
@@ -31,11 +30,27 @@
                     case 1:
                         Console.Clear();
                         Console.WriteLine();
-                        InputUser(id, first_name, last_name, password);
+                        InputUser( first_name, last_name, password);
                         break;
                     case 2:
                         Console.Clear();
-                        TampilUser(id, first_name, last_name, password);
+                        TampilUser( first_name, last_name, password);
+                        Console.WriteLine();
+                        Console.WriteLine("Menu");
+                        Console.WriteLine("1. Edit User");
+                        Console.WriteLine("2. Delete User");
+                        Console.WriteLine("3. Back");
+                        int menu = Convert.ToInt32(Console.ReadLine());
+                        if (menu == 1)
+                        {
+                            EditUser(first_name, last_name, password);
+                        }else if (menu == 2)
+                        {
+                            Console.Clear();
+                        }else if (menu == 3)
+                        {
+                            Main(args);
+                        }
                         break;
                     case 3:
                         Console.Clear();
@@ -59,7 +74,7 @@
                 input2 = Convert.ToInt32(Console.ReadLine());
             } while (input2 != 2);
         }
-        static void InputUser(List<int> id, List<string> first_name, List<string> last_name, List<string> password)
+        static void InputUser(List<string> first_name, List<string> last_name, List<string> password)
         {
             //input first_name
             Console.Write("First Name : ");
@@ -73,21 +88,19 @@
             Console.Write("Password : ");
             password.Add(Console.ReadLine());
 
-            //input id
-            int idinput = first_name.Count + 1;
-            id.Add(idinput);
-
         }
-        static void TampilUser(List<int> id, List<string> first_name, List<string> last_name, List<string> password)
+        static void TampilUser(List<string> first_name, List<string> last_name, List<string> password)
         {
             Console.WriteLine("==SHOW USER==");
             Console.WriteLine("=============");
             for (int i = 0; i < first_name.Count; i++)
             {
-                Console.WriteLine("ID       : " + id);
+                int id = i + 1;
+                Console.WriteLine("ID       : " + id );
                 Console.WriteLine("Name     : " + first_name[i] + " " + last_name[i]);
                 Console.WriteLine("Username : " + last_name[i]);
                 Console.WriteLine("Password : " + password[i]);
+                Console.WriteLine();
             }
             Console.WriteLine("=============");
         }
@@ -96,7 +109,6 @@
             Console.WriteLine("==CARI AKUN==");
             Console.Write("Masukan Nama : ");
             string name = Console.ReadLine();
-
         }
         static void LoginUser ()
         {
@@ -109,6 +121,28 @@
             // input Password
             Console.Write("PASSWORD : ");
             string pass = Console.ReadLine();
+        }
+        static void EditUser(List<string> first_name, List<string> last_name, List<string> password)
+        {
+                
+            Console.WriteLine("Id Yang Ingin Di Ubah : ");
+            int cekid = Convert.ToInt32(Console.ReadLine());;
+            if (first_name.Count == cekid)
+            {
+                cekid -= 1;
+                Console.WriteLine("First Name : ");
+                first_name[cekid] = (Console.ReadLine());
+                Console.WriteLine("Last Name : ");
+                last_name[cekid] = (Console.ReadLine());
+                Console.WriteLine("Password : ");
+                password[cekid] = (Console.ReadLine());
+                Console.WriteLine("User Success to Edited!!!");
+            }
+            else
+            {
+                Console.WriteLine("Data Tidak Ditemukan");
+            }
+            Console.ReadLine();
         }
     }
 }
